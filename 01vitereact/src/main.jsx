bd1abf9413de45d976d9a1ccd332fc3667f84c2e
waqarAli <alisyedwaqar108@gmail.com>
2024-11-06 15:26:38 +0500

    trying the customImplementation of React in 01 dir

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 // function App() {
 //   return <h1> Chai aur React</h1>;
 // }
-
+const userName = "chai aur global charge";
 // const anotherElement = (
 //   <a href="https://google.com" target="_blank">
 //     visit google
@@ -19,10 +19,16 @@ import App from "./App.jsx";
 //   },
 //   children: "click me to visit Google",
 // };
+const reactElement = React.createElement(
+  "h1",
+  {},
+  "created with customReact",
+  userName
+);
 
 // const reactElement = React.createElement(
 //   "a",
 //   { href: "https://google.com", target: "_blank" },
 //   "Click me to visit google"
 // );
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
