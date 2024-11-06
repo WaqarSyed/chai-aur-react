618daec532f4c37d7352c86623ced3866d15be0d
waqarAli <alisyedwaqar108@gmail.com>
2024-11-06 14:54:07 +0500

    how react creates elements and the tree

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,25 +1,22 @@
-function renderElement(reactElement, container) {
+function customRender(reactElement, container) {
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
-  /*domElement.setAttribute("href", reactElement.props.href);
-    domElement.setAttribute("target", reactElement.props.target);
-  
-    container.appendChild(domElement);*/
-
   for (const prop in reactElement.props) {
     if (prop === "children") continue;
     domElement.setAttribute(prop, reactElement.props[prop]);
   }
+  domElement.innerHTML = reactElement.children;
+
   container.appendChild(domElement);
 }
+
 const reactElement = {
   type: "a",
   props: {
     href: "https://google.com",
     target: "_blank",
   },
-  children: "click me to visit Google",
+  children: "Click me to visit your best search engine ever",
 };
-const mainContainer = document.getElementById("root");
-
-renderElement(reactElement, mainContainer);
+const root = document.getElementById("root");
+customRender(reactElement, root);
